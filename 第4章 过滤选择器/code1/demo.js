6b97c40a02da7dcfa3741f435c21c51063e407e9
ssggithub <13691897490@163.com>
2017-02-27 14:23:42 +0800

    过滤选择器

---
+++ 第4章 过滤选择器/code1/demo.js
@@ -0,0 +1,67 @@
+﻿
+
+$(function () {
+	
+	//$('li:first').css('background', '#ccc');
+	//$('li:last').css('background', '#ccc');
+	//$('#box li:last').css('background', '#ccc');
+	//$('ul:first li:last').css('background', '#ccc');
+	//$('li:not(.red)').css('background', '#ccc');
+	//$('li:even').css('background', '#ccc');
+	//$('li:odd').css('background', '#ccc');
+	//$('li:eq(2)').css('background', '#ccc');
+	//$('li:eq(-2)').css('background', '#ccc');
+	//$('li:gt(3)').css('background', '#ccc');
+	//$('li:lt(2)').css('background', '#ccc');
+	//$('div :header').css('background', '#ccc');
+	//$('input').get(0).focus();
+	//$(':focus').css('background', 'red');
+	
+	//$('li').first().css('background', '#ccc');
+	//$('li').last().css('background', '#ccc');
+	//$('li').not('.red').css('background', '#ccc');
+	//$('li').eq(2).css('background', '#ccc');
+	//$('div:contains("ycku.com")').css('background', '#ccc');
+	//$('div:empty').css('background', '#ccc').css('height','20px');
+	//$('ul:has(.red)').css('background', '#ccc');
+	//$('div:parent').css('background', '#ccc');
+	//$('ul').has('.red').css('background', '#ccc');
+	
+	//alert($('li').parent().size());
+	//alert($('li').parent().get(0));
+	//$('li').parent().css('background', '#ccc');
+	//$('li').parents().css('background', '#ccc');
+	//$('li').parentsUntil('body').css('background', '#ccc');
+	
+	//alert($('div:hidden').size());
+	//$('div:hidden').css('background', '#ccc').show(1000);
+	//alert($('div:visible').size());
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 第4章 过滤选择器/code2/demo.js
@@ -0,0 +1,73 @@
+﻿
+
+$(function () {
+	
+	//$('li:first-child').css('background', '#ccc');
+	//$('li:last-child').css('background', '#ccc');
+	//$('li:only-child').css('background', '#ccc');
+	//$('li:nth-child(even)').css('background', '#ccc');
+	//$('li:nth-child(odd)').css('background', '#ccc');
+	//$('li:nth-child(5n)').css('background', '#ccc');
+	//$('li:nth-child(4n+1)').css('background', '#ccc');
+	
+	//alert($('.red').is('li'));
+	//alert($('.red').is('div'));
+	//alert($('.red').is($('li')));
+	//alert($('.red').is($('div')));
+	//alert($('.red').is($('li').get(2)));
+	//alert($('.red').is($('li').eq(2)));
+	
+	//alert($('.red').is(function () {
+	//	return $(this).attr('title') == '列表3';
+	//}));
+	
+	//注意，必须使用$(this)来表示要判断的元素，否则，不管function里面是否返回true或false都和调用的元素无关了
+	
+	//alert($('li').eq(2).hasClass('red'));
+	
+	//$('li').slice(5,7).css('background', '#ccc');
+	//$('li').slice(2).css('background', '#ccc');
+	//$('li').slice(0, -2).css('background', '#ccc');
+	//$('li').slice(2, -2).css('background', '#ccc');
+	
+	//alert($('#box').find('li').end().get(0));
+	//alert($('#box').find('li').parent().get(0));
+	
+	//$('#box').next('ul').end().css('background', '#ccc');
+	
+	//alert($('div:first').children().size());
+	//alert($('div:first').contents().size());
+	
+	//$('li').filter('.red, :first-child, :last-child').css('background', '#ccc');
+	
+	$('li').filter(function () {
+		return $(this).attr('class') == 'red' && $(this).attr('title') == '列表3';
+	}).css('background', '#ccc');
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
