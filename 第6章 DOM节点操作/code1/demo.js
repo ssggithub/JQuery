43193d14c213729f9024b053869f48cc6fe48493
ssggithub <13691897490@163.com>
2017-03-12 20:51:03 +0800

    第6章 DOM节点操作

---
+++ 第6章 DOM节点操作/code1/demo.js
@@ -0,0 +1,63 @@
+﻿
+
+$(function () {
+
+	/*
+	var box = $('<div id="box">节点</div>');		//创建节点
+	$('body').append(box);								//插入节点
+	*/
+	
+	//$('div').append('<strong>DOM</strong>');
+	//$('div').append(function (index, html) {
+	//	return '<strong>DOM</strong>' + index + html;
+	//});
+	
+	//$('strong').appendTo('div');								//移入操作，不需要创建节点
+	
+	//$('div').prepend('<strong>DOM</strong>');
+	//$('div').prepend(function (index, html) {
+	//	return '<strong>DOM</strong>' + index + html;
+	//});
+	
+	//$('strong').prependTo('div');	
+	
+	//$('div').after('<strong>DOM</strong>');
+	//$('div').after(function (index, html) {
+	//	return '<strong>DOM</strong>' + index + html;
+	//});
+	
+	//$('div').before('<strong>DOM</strong>');
+	//$('div').before(function (index, html) {
+	//	return '<strong>DOM</strong>' + index + html;
+	//});
+	
+	//$('strong').insertAfter('div');	
+	//$('strong').insertBefore('div');	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 第6章 DOM节点操作/code2/demo.js
@@ -0,0 +1,74 @@
+﻿
+
+$(function () {
+
+	//$('div').wrap('<strong class="b"></strong>');
+	//$('div').wrap('<strong />');
+	//$('div').wrap('<strong>123</strong>');
+	//$('div').wrap('<strong><em></em></strong>');
+	
+	//$('div').wrap($('strong').get(0));
+	//$('div').wrap(document.createElement('strong'));
+	
+	//$('div').wrap(function (index) {
+	//	return '<strong>' + index + '</strong>'
+	//});
+	
+	//$('div').wrap('<strong><em></em></strong>');
+	//$('div').unwrap();
+	//$('div').unwrap();
+	
+	//$('div').wrap('<strong></strong>');
+	//$('div').wrapAll('<strong></strong>');
+	//$('div').wrapAll(document.createElement('strong'));
+	
+	//$('div').wrap('<strong></strong>');
+	//$('div').wrapInner('<strong></strong>');
+	//$('div').wrapInner(document.createElement('strong'));
+	//$('div').wrapInner(function (index) {
+	//	return '<strong>' + index + '</strong>'
+	//});
+	
+	$('div').click(function () {
+		alert('ycku.com');
+	});
+	
+	//$('div').clone(true).appendTo('body');
+	//$('div').remove();
+	//$('div').remove('.box');
+	
+	//alert($('div').remove().get(0));
+	//$('div').remove().appendTo('body');
+	//$('div').detach().appendTo('body');
+	//$('div').empty();
+	
+	//$('div').replaceWith('<span>DOM</span>');
+	$('<span>DOM</span>').replaceAll('div');
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
