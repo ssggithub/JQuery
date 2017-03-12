dcb7ec33e0ef646cf38ccdfec1d8d80122ca05e2
ssggithub <13691897490@163.com>
2017-03-12 21:14:54 +0800

    第8章 基础事件

---
+++ 第8章 基础事件/code1/demo.js
@@ -0,0 +1,139 @@
+﻿
+
+$(function () {
+
+	/*
+	$('input').bind('click', function () {
+		alert('弹窗！');
+	});
+
+	
+	$('input').bind('click', fn);
+	
+	function fn() {
+		alert('处理函数！')
+	}
+
+	
+	$('input').bind('click mouseover', function () {
+		alert('弹窗！');
+	});
+
+	
+	$('input').bind('mouseover mouseout', function () {
+		$('div').html(function (index, value) {
+			return value + '1';
+		});
+	});
+
+	
+	$('input').bind({
+		mouseover : function () {
+			alert('移入');
+		},
+		mouseout : function () {
+			alert('移出');
+		}
+	});
+
+	
+	$('input').bind({
+		'mouseover' : function () {
+			alert('移入');
+		},
+		'mouseout' : function () {
+			alert('移出');
+		}
+	});
+
+	
+	$('input').bind('click mouseover', function () {
+		alert('弹窗！');
+	});
+
+	
+	$('input').bind('click', fn1);
+	$('input').bind('click', fn2);
+	
+	function fn1() {
+		alert('fn1');
+	}
+	
+	function fn2() {
+		alert('fn2');
+	}
+	
+	//$('input').unbind();		//删除全部事件
+	//$('input').unbind('click');	//只删除click事件
+	$('input').unbind('click', fn2);  //删除click事件绑定了fn2的
+
+	
+	$('input').click(function () {
+		alert('单击');
+	});
+	
+	$('input').dblclick(function () {
+		alert('双击');
+	});
+	
+	$('input').mousedown(function () {
+		alert('鼠标左键按下');
+	});
+	
+	$('input').mouseup(function () {
+		alert('鼠标左键按下弹起');
+	});
+	
+	$(window).unload(function () {			//一般unload卸载页面新版浏览器应该是不支持的，获取要设置一个。
+		alert('1');										//一般用于清理工作。
+	});
+	
+	$(window).resize(function () {			
+		alert('文档改变了');									
+	});
+	
+	$(window).scroll(function () {			
+		alert('滚动条改变了');									
+	});
+	$('input').select(function () {
+		alert('文本选定');
+	});
+	
+	$('input').change(function () {
+		alert('文本改变');
+	});
+	*/
+
+	$('form').submit(function () {
+		alert('表单提交！');
+	});
+	
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 第8章 基础事件/code2/demo.js
@@ -0,0 +1,143 @@
+﻿
+
+$(function () {
+
+	/*
+	$('div').mouseover(function () {
+		$(this).css('background', 'red');
+	}).mouseout(function () {
+		$(this).css('background', 'green');
+	});
+	
+	$('div').mouseenter(function () {
+		$(this).css('background', 'red');
+	}).mouseleave(function () {
+		$(this).css('background', 'green');
+	});
+	
+	$('div').mouseover(function() {					//over会触发子节点
+		$('strong').html(function (index, value) {
+			return value + '1';
+		});
+	});
+	
+	$('div').mouseenter(function() {				//enter不会触发子节点
+		$('strong').html(function (index, value) {
+			return value + '1';
+		});
+	});
+	
+	$('div').mouseout(function() {					
+		$('strong').html(function (index, value) {
+			return value + '1';
+		});
+	});
+	
+	$('div').mouseleave(function() {					
+		$('strong').html(function (index, value) {
+			return value + '1';
+		});
+	});
+	
+	$('input').keydown(function () {
+		alert('键盘');
+	});
+	
+	$('input').keyup(function () {
+		alert('键盘');
+	});
+	
+	$('input').keydown(function (e) {
+		alert(e.keyCode);
+	});
+	
+	$('input').keypress(function (e) {
+		alert(e.charCode);
+	});
+	
+	$('input').focus(function () {
+		alert('光标激活');
+	});
+	
+	$('input').blur(function () {
+		alert('光标丢失');
+	});
+	
+	$('input').focusout(function () {
+		alert('光标丢失');
+	});
+	
+	$('div').focus(function () {			//focus和blur必须是当前元素才能激活
+		alert('光标激活');
+	});
+	
+	$('div').focusin(function () {			//focusin和focusout可以是子元素激活
+		alert('光标激活');
+	});
+	
+	$('div').focusout(function () {			
+		alert('光标丢失');
+	});
+	
+	$('div').hover(function () {
+		$(this).css('background', 'red');
+	}, function () {
+		$(this).css('background', 'green');
+	});
+	
+	//$('div').toggle('slow');
+	
+	$('div').toggle(function () {
+		$(this).css('background', 'red');
+	}, function () {
+		$(this).css('background', 'blue');
+	}, function () {
+		$(this).css('background', 'green');
+	});
+
+	*/
+
+	
+	var flag = 1;
+	$('div').click(function () {
+		if (flag == 1) {
+			$(this).css('background', 'red');
+			flag = 2;
+		} else if (flag == 2) {
+			$(this).css('background', 'blue');
+			flag = 3;
+		} else if (flag == 3) {
+			$(this).css('background', 'green');
+			flag = 1;
+		}
+	});
+	
+
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
